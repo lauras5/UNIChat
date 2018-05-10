f9b09d2551b45e8f63f2a0b4a09b3c2e291bb08c
Laura Salcido <idlaurasalcido@gmail.com>
2018-05-09 19:41:30 -0700

    notes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,21 @@
 USE unichat_db;
 
 INSERT INTO users
-SET name = 'Barb Walter', password = 'blahblah5', email = 'admin@unichat.com', dorm = 0, clearance_level='admin';
+SET name = 'Laura Salcido', password = 'asdf', email = 'laura@admin.com', dorm = 0, clearance_level='admin';
 
 INSERT INTO users
-SET name = 'Mary Spritz', password = 'password1', email = 'mspritz@unichat.com', dorm = 1, clearance_level='ra';
+SET name = 'Bree Nelson', password = 'asdf', email = 'Bree@ra.com', dorm = 1, clearance_level='ra';
 
 INSERT INTO users
 SET name = 'Alice Chambers', password = 'password2', email = 'mspritz@unichat.com', dorm = 2, clearance_level='ra';
 
 INSERT INTO users
 SET name = 'Michelle James', password = 'password3', email = 'mjames@unichat.com', dorm = 3, clearance_level='ra';
+
+INSERT INTO users
+SET name = 'Jordan Novoa', password = 'asdf', email = 'jordan@student.com', dorm = 1, clearance_level = 'student';
+
+INSERT INTO users
+SET name = 'Rifa Sayed', password = 'asdf', email = 'rifa@student.com', dorm = 2, clearance_level = 'student';
 
 
