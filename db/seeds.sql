20b5d0e32c21f1d97e01308d163027189f0d25ab
jnovoa29 <jordan.novoa15@gmail.com>
2018-05-09 21:43:31 -0700

    devpage links, feed html css visual edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,12 +4,12 @@ INSERT INTO users
 SET name = 'Barb Walter', password = 'blahblah5', email = 'admin@unichat.com', dorm = 0, clearance_level='admin';
 
 INSERT INTO users
-SET name = 'Mary Spritz', password = 'password1', email = 'mspritz@unichat.com', dorm = 1, clearance_level='ra';
+SET name = 'Mary Spritz', password = 'password1', email = 'mspritz@unichat.com', dorm = 1, clearance_level='student';
 
 INSERT INTO users
 SET name = 'Alice Chambers', password = 'password2', email = 'mspritz@unichat.com', dorm = 2, clearance_level='ra';
 
 INSERT INTO users
-SET name = 'Michelle James', password = 'password3', email = 'mjames@unichat.com', dorm = 3, clearance_level='ra';
+SET name = 'Michelle James', password = 'password3', email = 'mjames@unichat.com', dorm = 3, clearance_level='student';
 
 
